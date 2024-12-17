e6be350316bc3a57268e3e4e333d56380be78c9a
Vu.PM <Vu.PM@iigvietnam.edu.vn>
2024-12-17 15:23:29 +0700

    ud

--- src/pages/user/profile.tsx
+++ src/pages/user/profile.tsx
@@ -1,206 +1,221 @@
-import React, { useEffect, useState } from 'react'
-import { Box, Icon, List, Text, useNavigate } from 'zmp-ui'
+import React, {useEffect, useState} from 'react'
+import {Box, Icon, List, Text, useNavigate} from 'zmp-ui'
 import Container from '../../components/layout/Container'
-import { HiOutlineFlag, HiOutlineShoppingCart, HiOutlineUser } from 'react-icons/hi'
-import { useRecoilState, useRecoilValue } from 'recoil'
-import { authState } from '../../states/auth'
+import {HiOutlineFlag, HiOutlineShoppingCart, HiOutlineUser} from 'react-icons/hi'
+import {useRecoilState, useRecoilValue} from 'recoil'
+import {authState} from '../../states/auth'
 import useSetHeader from '../../hooks/useSetHeader'
-import { showOAWidget } from 'zmp-sdk/apis'
-import { getPhoneNumberUser } from '../../services/zalo'
-import { isFromSettingState, isMappingState } from '../../state'
-import { createShortcut } from 'zmp-sdk/apis'
-import { openWebview } from 'zmp-sdk/apis'
-import { convertPrice } from '../../utils'
+import {showOAWidget} from 'zmp-sdk/apis'
+import {getPhoneNumberUser} from '../../services/zalo'
+import {isFromSettingState, isMappingState} from '../../state'
+import {createShortcut} from 'zmp-sdk/apis'
+import {openWebview} from 'zmp-sdk/apis'
+import {convertPrice} from '../../utils'
+
 const openUrlInWebview = async () => {
-	try {
-		await openWebview({
-			url: 'https://zalo.me/0963559840',
-			config: {
-				style: 'bottomSheet',
-				leftButton: 'back',
-			},
-		})
-	} catch (error) {
-		// xử lý khi gọi api thất bại
-		console.log(error)
-	}
+    try {
+        await openWebview({
+            url: 'https://zalo.me/0963559840',
+            config: {
+                style: 'bottomSheet',
+                leftButton: 'back',
+            },
+        })
+    } catch (error) {
+        // xử lý khi gọi api thất bại
+        console.log(error)
+    }
 }
 
 const createMiniAppShortcut = async () => {
-	try {
-		await createShortcut({
-			params: {
-				utm_source: 'shortcut',
-			},
-		})
-	} catch (error) {
-		// xử lý khi gọi api thất bại
-		console.log(error)
-	}
+    try {
+        await createShortcut({
+            params: {
+                utm_source: 'shortcut',
+            },
+        })
+    } catch (error) {
+        // xử lý khi gọi api thất bại
+        console.log(error)
+    }
 }
 
-const { Item } = List
+const {Item} = List
 const UserProfile = () => {
-	const navigate = useNavigate()
-	const authDt = useRecoilValue(authState)
-	const setHeader = useSetHeader()
-	const [point, setPoint] = useState(0)
-	const [phoneNumber, setPhoneNumber] = useState<string | null>(null)
-	const [isMapping, setIsMapping] = useRecoilState<boolean>(isFromSettingState)
-	useEffect(() => {
-		setHeader({
-			customTitle: 'Trang cá nhân',
-			hasLeftIcon: true,
-			type: 'secondary',
-			showBottomBar: true,
-		})
-		showOAWidget({
-			id: 'oaWidget',
-			guidingText: 'Nhận thông báo khuyến mãi mới nhất từ cửa hàng',
-			color: '#0068FF',
-			onStatusChange: (status) => {
-				console.log(status)
-			},
-		})
-		// const savedPhoneNumber = sessionStorage.getItem("phoneNumber");
-		//         if (savedPhoneNumber) {
-		//             setPhoneNumber(savedPhoneNumber);
-		//         }else{
-		//             getPhoneNumberUser();
-		//         }
-	}, [])
+    const navigate = useNavigate()
+    const authDt = useRecoilValue(authState)
+    const setHeader = useSetHeader()
+    const [point, setPoint] = useState(0)
+    const [phoneNumber, setPhoneNumber] = useState<string | null>(null)
+    const [isMapping, setIsMapping] = useRecoilState<boolean>(isFromSettingState)
+    useEffect(() => {
+        setHeader({
+            customTitle: 'Trang cá nhân',
+            hasLeftIcon: true,
+            type: 'secondary',
+            showBottomBar: true,
+        })
+        showOAWidget({
+            id: 'oaWidget',
+            guidingText: 'Nhận thông báo khuyến mãi mới nhất từ cửa hàng',
+            color: '#0068FF',
+            onStatusChange: (status) => {
+                console.log(status)
+            },
+        })
+        // const savedPhoneNumber = sessionStorage.getItem("phoneNumber");
+        //         if (savedPhoneNumber) {
+        //             setPhoneNumber(savedPhoneNumber);
+        //         }else{
+        //             getPhoneNumberUser();
+        //         }
+    }, [])
 
-	useEffect(() => {
-		const userId = authDt?.profile?.id
-		if (userId) {
-			fetch(`https://quequan.vn:8081/customer/zalo-customer-point?userid=${userId}`)
-				.then((response) => {
-					return response.json()
-				})
-				.then((result) => {
-					console.log(result)
-					if (result?.point) setPoint(result?.point)
-				})
-				.catch((error) => console.log(JSON.stringify(error)))
-		}
-	}, [authDt?.profile?.id])
+    useEffect(() => {
+        const userId = authDt?.profile?.id
+        if (userId) {
+            fetch(`https://quequan.vn:8081/customer/zalo-customer-point?userid=${userId}`)
+                .then((response) => {
+                    return response.json()
+                })
+                .then((result) => {
+                    console.log(result)
+                    if (result?.point) setPoint(result?.point)
+                })
+                .catch((error) => console.log(JSON.stringify(error)))
+        }
+    }, [authDt?.profile?.id])
 
-	return (
-		<Container className={'  zui-container-background-color'}>
-			<Box m={4} p={0} className={'rounded-lg bg-white'}>
-				<List>
-					<Item
-						title='Điểm tích luỹ'
-						prefix={<HiOutlineUser size={20} />}
-						className={'text-sm m-0'}
-						suffix={
-							<div className='flex flex-row gap-2 items-center'>
-								<Text className='text-base text-green-500 font-semibold'>{convertPrice(point ?? 0)}</Text>
-								<Icon icon='zi-chevron-right' />
-							</div>
-						}
-						onClick={() => {
-							setIsMapping(true)
-							navigate('/history-points')
-						}}
-					/>
-					<Item
-						title='Thông tin tài khoản'
-						prefix={<HiOutlineUser size={20} />}
-						className={'text-sm m-0'}
-						suffix={<Icon icon='zi-chevron-right' />}
-						onClick={() => {
-							setIsMapping(true)
-							navigate('/user-info')
-						}}
-					/>
-					<Item
-						title='Địa chỉ đã lưu'
-						prefix={<HiOutlineFlag size={20} />}
-						suffix={<Icon icon='zi-chevron-right' />}
-						className={'text-sm m-0'}
-						onClick={() => {
-							setIsMapping(true)
-							navigate('/my-addresses/profile')
-						}}
-					/>
-					<Item
-						title='Lịch sử đơn hàng'
-						prefix={<HiOutlineShoppingCart size={20} />}
-						suffix={<Icon icon='zi-chevron-right' />}
-						className={'text-sm m-0'}
-						onClick={() => {
-							setIsMapping(true)
-							navigate('/my-orders')
-						}}
-					/>
-					<Item
-						title='Mã giới thiệu'
-						prefix={<HiOutlineUser size={20} />}
-						className={'text-sm m-0'}
-						suffix={<Icon icon='zi-chevron-right' />}
-						onClick={() => {
-							setIsMapping(true)
-							navigate('/user-referral')
-						}}
-					/>
-				</List>
-			</Box>
-			<Box m={4} p={0} className={'rounded-lg bg-white'}>
-				<List>
-					<Item
-						title='Chính sách riêng tư'
-						className={'text-sm m-0'}
-						suffix={<Icon icon='zi-chevron-right' />}
-						onClick={() => {
-							setIsMapping(true)
-							navigate('/detail-new/3')
-						}}
-					/>
-					<Item
-						title='Điều khoản dịch vụ'
-						suffix={<Icon icon='zi-chevron-right' />}
-						className={'text-sm m-0'}
-						onClick={() => {
-							setIsMapping(true)
-							navigate('/detail-new/4')
-						}}
-					/>
-					<Item
-						title='Hướng dẫn sử dụng'
-						suffix={<Icon icon='zi-chevron-right' />}
-						className={'text-sm m-0'}
-						onClick={() => {
-							setIsMapping(true)
-							navigate('/detail-new/1')
-						}}
-					/>
-					<Item
-						title='Thêm Quế Quân vào màn hình chính'
-						suffix={<Icon icon='zi-share-external-1' />}
-						className={'text-sm m-0'}
-						onClick={() => {
-							createMiniAppShortcut()
-						}}
-					/>
-				</List>
-			</Box>
-			<Box m={4} p={0} className='rounded-lg bg-white'>
-				<div id='oaWidget' />
-			</Box>
-			<Box m={4} p={0} className='rounded-lg bg-white'>
-				<List>
-					<Item
-						title='Liên hệ với nhà phát triển ứng dụng'
-						suffix={<Icon icon='zi-chat' />}
-						className={'text-sm m-0'}
-						onClick={() => {
-							openUrlInWebview()
-						}}
-					/>
-				</List>
-			</Box>
-		</Container>
-	)
+    return (
+        <Container className={'  zui-container-background-color'}>
+            <Box m={4} p={0}
+                 onClick={() => {
+                     setIsMapping(true)
+                     navigate('/history-points')
+                 }}
+                 className={'rounded-lg'} style={{backgroundColor: '#088c4c'}}>
+                <div className="text-center p-4">
+                    <Text size={"large"} className={' text-white font-semibold'}>
+                        Hoa hồng có thể nhận
+                    </Text>
+                    <Text style={{ fontSize: '30px' }} className={'text-white mt-2 mb-4 font-semibold'}>0đ</Text>
+                </div>
+            </Box>
+            <Box m={4} p={0} className={'rounded-lg bg-white'}>
+                <List>
+                    {/*<Item*/}
+                    {/*    title='Điểm tích luỹ'*/}
+                    {/*    prefix={<HiOutlineUser size={20}/>}*/}
+                    {/*    className={'text-sm m-0'}*/}
+                    {/*    suffix={*/}
+                    {/*        <div className='flex flex-row gap-2 items-center'>*/}
+                    {/*            <Text*/}
+                    {/*                className='text-base text-green-500 font-semibold'>{convertPrice(point ?? 0)}</Text>*/}
+                    {/*            <Icon icon='zi-chevron-right'/>*/}
+                    {/*        </div>*/}
+                    {/*    }*/}
+                    {/*    onClick={() => {*/}
+                    {/*        setIsMapping(true)*/}
+                    {/*        navigate('/history-points')*/}
+                    {/*    }}*/}
+                    {/*/>*/}
+                    <Item
+                        title='Thông tin tài khoản'
+                        prefix={<HiOutlineUser size={20}/>}
+                        className={'text-sm m-0'}
+                        suffix={<Icon icon='zi-chevron-right'/>}
+                        onClick={() => {
+                            setIsMapping(true)
+                            navigate('/user-info')
+                        }}
+                    />
+                    <Item
+                        title='Địa chỉ đã lưu'
+                        prefix={<HiOutlineFlag size={20}/>}
+                        suffix={<Icon icon='zi-chevron-right'/>}
+                        className={'text-sm m-0'}
+                        onClick={() => {
+                            setIsMapping(true)
+                            navigate('/my-addresses/profile')
+                        }}
+                    />
+                    <Item
+                        title='Lịch sử đơn hàng'
+                        prefix={<HiOutlineShoppingCart size={20}/>}
+                        suffix={<Icon icon='zi-chevron-right'/>}
+                        className={'text-sm m-0'}
+                        onClick={() => {
+                            setIsMapping(true)
+                            navigate('/my-orders')
+                        }}
+                    />
+                    <Item
+                        title='Mã giới thiệu'
+                        prefix={<HiOutlineUser size={20}/>}
+                        className={'text-sm m-0'}
+                        suffix={<Icon icon='zi-chevron-right'/>}
+                        onClick={() => {
+                            setIsMapping(true)
+                            navigate('/user-referral')
+                        }}
+                    />
+                </List>
+            </Box>
+            <Box m={4} p={0} className={'rounded-lg bg-white'}>
+                <List>
+                    <Item
+                        title='Chính sách riêng tư'
+                        className={'text-sm m-0'}
+                        suffix={<Icon icon='zi-chevron-right'/>}
+                        onClick={() => {
+                            setIsMapping(true)
+                            navigate('/detail-new/3')
+                        }}
+                    />
+                    <Item
+                        title='Điều khoản dịch vụ'
+                        suffix={<Icon icon='zi-chevron-right'/>}
+                        className={'text-sm m-0'}
+                        onClick={() => {
+                            setIsMapping(true)
+                            navigate('/detail-new/4')
+                        }}
+                    />
+                    <Item
+                        title='Hướng dẫn sử dụng'
+                        suffix={<Icon icon='zi-chevron-right'/>}
+                        className={'text-sm m-0'}
+                        onClick={() => {
+                            setIsMapping(true)
+                            navigate('/detail-new/1')
+                        }}
+                    />
+                    <Item
+                        title='Thêm Quế Quân vào màn hình chính'
+                        suffix={<Icon icon='zi-share-external-1'/>}
+                        className={'text-sm m-0'}
+                        onClick={() => {
+                            createMiniAppShortcut()
+                        }}
+                    />
+                </List>
+            </Box>
+            <Box m={4} p={0} className='rounded-lg bg-white'>
+                <div id='oaWidget'/>
+            </Box>
+            <Box m={4} p={0} className='rounded-lg bg-white'>
+                <List>
+                    <Item
+                        title='Liên hệ với nhà phát triển ứng dụng'
+                        suffix={<Icon icon='zi-chat'/>}
+                        className={'text-sm m-0'}
+                        onClick={() => {
+                            openUrlInWebview()
+                        }}
+                    />
+                </List>
+            </Box>
+        </Container>
+    )
 }
 export default UserProfile
